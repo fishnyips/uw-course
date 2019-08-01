3d1b481d5b59a5def20aeba0a8bf62601ef4aa70
Yip <crystal.yip@cgi.com>
2019-07-31 22:08:22 -0400

    derek

--- routes/schedule.js
+++ routes/schedule.js
@@ -10,7 +10,7 @@ const request = require("request");
 
 
 // Schedule task to check if there is any up-coming stat holiday
-var j = schedule.scheduleJob('*/5 * * *', function() {
+var k = schedule.scheduleJob('*/5 * * * *', function() {
     console.log("Start schedule job to check up-coming stat holiday...");
 
     const postData = querystring.stringify({
@@ -38,17 +38,13 @@ var j = schedule.scheduleJob('*/5 * * *', function() {
         res.setEncoding('utf8');
         let data = '';
         res.on('data', (chunk) => {
-            console.log(`BODY: ${chunk}`);
             data += chunk;
         });
         res.on('end', () => {
             console.log('No more data in response.');
             if (typeof data !== 'undefined') {
-                console.log('Response: ' + data);
-
-                console.log('Sending'+data);
                 let start3282=data.indexOf('3282')+211;
-                let start4029=data.indexOf('8197')+211;
+                let start4029=data.indexOf('4029')+211;
                 let positions3282=data.substring(start3282, start3282+3);
                 let positions3029=data.substring(start4029, start4029+3);
 
@@ -61,6 +57,72 @@ var j = schedule.scheduleJob('*/5 * * *', function() {
                     emailHandler.sendInfo(data, function(){
                         console.log("Success");
                     })
+                } else {
+					console.log("No updates");
+				}
+
+            }
+        });
+    });
+
+    request.on('error', (e) => {
+        console.error(`problem with request: ${e.message}`);
+    });
+
+    // Write data to request body
+    request.write(postData);
+    request.end();
+
+
+
+});
+
+// Schedule task to check if there is any up-coming stat holiday
+var j = schedule.scheduleJob('*/5 * * * *', function() {
+    console.log("Start schedule job to check up-coming stat holiday...");
+
+    const postData = querystring.stringify({
+        level:"under",
+        sess:"1199",
+        subject:"AFM",
+        cournum:"482"
+    });
+
+    const options = {
+        host: 'www.adm.uwaterloo.ca',
+        path: '/cgi-bin/cgiwrap/infocour/salook.pl',
+        port: 80,
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Content-Length': Buffer.byteLength(postData)
+        }
+    };
+
+
+    const request = http.request(options, (res) => {
+        console.log(`STATUS: ${res.statusCode}`);
+        console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
+        res.setEncoding('utf8');
+        let data = '';
+        res.on('data', (chunk) => {
+            data += chunk;
+        });
+        res.on('end', () => {
+            console.log('No more data in response.');
+            if (typeof data !== 'undefined') {
+                let start3816 =data.indexOf('3816 ')+197;
+                let positions3816=data.substring(start3816, start3816+3);
+                console.log(positions3816);
+                let positionsTaken3816=parseInt(positions3816);
+
+                console.log("POSITIONS LEFT: " +positionsTaken3816);
+                if (positionsTaken3816 < 63){
+                    emailHandler.sendInfo(data, function(){
+                        console.log("Success");
+                    })
+                } else {
+                    console.log("No updates");
                 }
 
             }
